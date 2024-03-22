0f6fbe6bbdaa2af7e4223cdaa9be08be20f9ef62
Rajan kumar jha <rajyotiroshan@gmail.com>
2024-03-22 15:42:43 +0000

    Inheriance#final

--- Start/Ch1/Inheritance/Program.cs
+++ Start/Ch1/Inheritance/Program.cs
@@ -4,18 +4,29 @@
 // Instantiate some objects
 Circle c = new Circle(10);
 Rectangle r = new Rectangle(10,20);
+Square s = new Square(10);
 
 // TODO: Exercise the ToString() method
-
+Console.WriteLine($"{c}");
 
 // TODO: Use the "is" operator to test an object type
-
-
+//Console.WriteLine($"{c is Shape2D}");
+//Console.WriteLine($"{c is Rectangle}");
 // TODO: Call the GetArea() function on each one
+Console.WriteLine(c.GetArea());
+Console.WriteLine(r.GetArea());
+
+Console.WriteLine(s.GetArea());
 
 
 // TODO: Print the area of each shape
 
+void PrinArea(Shape2D s){
 
+    Console.WriteLine($"{s.GetArea()}");
+}
 // TODO: All of the classes derive from Shape2D, so we can treat each one
 // as an instance of the base class. 
+PrinArea(c);
+PrinArea(s);
+PrinArea(r);
--- Start/Ch1/Inheritance/shapes.cs
+++ Start/Ch1/Inheritance/shapes.cs
@@ -6,10 +6,13 @@ class Shape2D {
     public Shape2D() {}
 
     // TODO: Methods must be marked as "virtual" to allow overriding
-
+    public virtual float GetArea(){
+        return 0.0f;
+    }
 
     // TODO: All classes in .NET derive from Object, so we get some methods for free
     // ToString() returns a string representation of the object
+    public override string ToString() =>  $"This Object is a '{GetType()}'";
     // GetType() returns the type of the object
 }
 
@@ -20,7 +23,10 @@ class Circle : Shape2D {
     }
 
     // TODO: Override the GetArea() function for the Circle
-
+    public override float GetArea()
+    {
+        return (3.14f*(radius*radius));
+    }
     int radius;
 }
 
@@ -32,9 +38,16 @@ class Rectangle : Shape2D {
     }
 
     // TODO: Override the GetArea() function for the Rectangle
-
+    public override float GetArea()
+    {
+        return (width * height)
+;    }
     int width;
     int height; 
 }
 
 // TODO: Derive a Square class that inherits from the base Rectangle
+class Square: Rectangle {
+    public Square(int side): base(side, side){} //init base class with side
+
+}
