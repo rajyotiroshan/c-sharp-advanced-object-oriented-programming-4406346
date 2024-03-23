fe024ec4fc6c57e835b9c800f1c32f2a3c67aaa1
Rajan kumar jha <rajyotiroshan@gmail.com>
2024-03-23 17:38:44 +0000

    readonly

--- Start/Ch2/ReadonlyProps/ReadOnly.cs
+++ Start/Ch2/ReadonlyProps/ReadOnly.cs
@@ -1,9 +1,12 @@
 // Example file for Advanced C#: Object Oriented Programming by Joe Marini
 // Using the readonly modifier for class members
-
+//Notes::readonly can be used in
+//1. get initi of empy
+//2. in constrctor 
+//const vs readly, const-> at compile time, readonly at runtime during constructor init. so at later after init.
 public class Book {
     // Declare some private fields
-    private string _ISBN = "";
+    private readonly string _ISBN = "";
     private string _title = "";
     private string _author = "";
 
@@ -14,7 +17,7 @@ public class Book {
     }
 
     public void Update(string ISBN, string Title, string Author) {
-        _ISBN = ISBN;
+       // _ISBN = ISBN;
         _title = Title;
         _author = Author;
     }
