ef549f572eb01908e7284ee835fe6e32604ac3c1
Rajan kumar jha <rajyotiroshan@gmail.com>
2024-04-02 15:21:14 +0000

    static method, static method overloading, static metod with instance variable

--- Start/Ch3/StaticMethods/StaticMethods.cs
+++ Start/Ch3/StaticMethods/StaticMethods.cs
@@ -11,6 +11,15 @@ public class Temperature {
     }
 
     // TODO: static methods use the 'static' modifier in their declaration
+    public static double FtoC(double FTemp){
+        double cTemp = (FTemp - 32.0)* (5.0/9.0);
+        return cTemp;
+    }
+
+    public static double CtoF(double CTemp){
+        double FTemp = (CTemp * (9.0/5.0)) + 32.0;
+        return FTemp;
+    }
 
     // Regular property implementation
     public double Temp { get; set; }
@@ -20,8 +29,12 @@ public class Temperature {
         return Temp >= ROOM_TEMP_LOWER_C && Temp <= ROOM_TEMP_UPPER_C;
     }
 
-    // This does not work - will cause a compile error
-    // public bool IsRoomTemp {
-    //     get => Temp >= ROOM_TEMP_LOWER_C && Temp <= ROOM_TEMP_UPPER_C;
-    // }
+    public static bool IsRoomTemp(double Temp){
+        return Temp >= ROOM_TEMP_LOWER_C && Temp <= ROOM_TEMP_UPPER_C; 
+    }
+
+    //This does not work - will cause a compile error
+//     public bool IsRoomTemp {
+//         get => Temp >= ROOM_TEMP_LOWER_C && Temp <= ROOM_TEMP_UPPER_C;
+//     }
 }
