fc662e2d4d733db550bc0892e863591e0f6eff05
Rajan kumar jha <rajyotiroshan@gmail.com>
2024-04-02 15:01:53 +0000

    inheritance with automatic exercise

--- Start/Ch2/Challenge/Employees.cs
+++ Start/Ch2/Challenge/Employees.cs
@@ -1,12 +1,49 @@
 // Example file for Advanced C#: Object Oriented Programming by Joe Marini
 // Solution to the Employee Class challenge
 
+using System.Transactions;
+
 public class Employee {
-    public Employee() {}
+
+    public Employee( ) {
+    }
 
     // YOUR CODE GOES HERE
+    public required int  ID {get;set;}
 
+    public required string Department {get; set;}
+
+    public required string FullName {get;set;}
+
+    public virtual void AdjustPay(decimal percentage){
+
+    }
     public override string ToString() => $"{ID}:{FullName}, {Department} ";
 }
 
 // DEFINE OTHER CLASSES HERE
+
+class HourlyEmployee : Employee {
+    public HourlyEmployee(){}
+
+    public decimal PayRate {
+        get;set;
+    }
+
+    public override void AdjustPay(decimal percentage)
+    {
+        PayRate += (PayRate * percentage);
+    }
+}
+class SalariedEmployee: Employee {
+  public SalariedEmployee(){
+
+  }
+
+  public decimal Salary {get; set;}
+
+    public override void AdjustPay(decimal percentage)
+    {
+        Salary += (Salary * percentage);
+    }
+}
