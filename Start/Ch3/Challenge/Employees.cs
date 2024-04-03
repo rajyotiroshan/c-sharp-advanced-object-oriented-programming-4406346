11574b57df5a1b17a4aa509c5aa63e88237e5eeb
Rajan kumar jha <rajyotiroshan@gmail.com>
2024-04-03 16:22:26 +0000

    static members, class, properties, constructor

--- Start/Ch3/Challenge/Employees.cs
+++ Start/Ch3/Challenge/Employees.cs
@@ -2,9 +2,21 @@
 // Solution to the Employee Class challenge
 
 public class Employee {
-    public Employee() {}
+    private static int _empCount = 0;
+    protected static int IDStart;
+    
+    static Employee() {
+        IDStart = 1000;
+    }
 
-    public required int ID {get; init;}
+    public Employee() {
+        Employee._empCount++;
+        ID = Employee.IDStart++;
+    }
+
+    public static int EmployeeCount { get => _empCount; }
+
+    public int ID {get; init;}
     public required string Department {get; set;}
     public required string FullName {get; set;}
 
